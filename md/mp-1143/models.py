d575d6c81c6baecbf1dc8a73baa289b17db731be
PythonFZ <fabian.zills@web.de>
2025-04-09 21:48:12 +0200

    update all nodes

--- md/mp-1143/models.py
+++ md/mp-1143/models.py
@@ -118,7 +118,6 @@ MODELS = {
     "orb-v2": ALL_MODELS["orb-v2"],
     "orb-v3": ALL_MODELS["orb-v3"],
     "mattersim": ALL_MODELS["mattersim"],
-    "orca": ALL_MODELS["orca"],
     "GRACE-2L-OMAT": ALL_MODELS["GRACE-2L-OMAT"],
     "chgnet": ALL_MODELS["chgnet"],
 }
